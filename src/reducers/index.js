12799e2759f8f2dffed064ca240520d2b2e9f002
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-09-14 13:59:19 -0700

    Build the two reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,18 @@
+// we need two reducres
+// Song list and Selected song reducers
+
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2:09" },
+    { title: "All Star", duration: "4:32" },
+    { title: "I want it", duration: "1:45" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG-SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
